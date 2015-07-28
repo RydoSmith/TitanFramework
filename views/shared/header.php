be6de72622851f79c1f52e365cdddcc0fd640804
RyanSmith <ryansmith13@outlook.com>
2015-07-28 17:30:49 +0100

    Added jquery and boostrap, make it look good

--- views/shared/header.php
+++ views/shared/header.php
@@ -14,8 +14,12 @@
 <link rel="apple-touch-icon" sizes="114x114" href="#">
 
 <link href='http://fonts.googleapis.com/css?family=Raleway:300,400,600,700,900' rel='stylesheet' type='text/css'>
-<link href="/public/css/style.css" rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="/public/css/bootstrap.min.css" />
+<link rel="stylesheet" href="/public/css/bootstrap-theme.min.css" />
+<link rel="stylesheet" href="/public/css/style.css" />
 
+<script src="/public/js/jquery-1.11.3.min.js"></script>
+<script src="/public/js/bootstrap.min.js"></script>
 
 
 
